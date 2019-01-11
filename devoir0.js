7a6972a9f95e380c86fbfe6c08663eece939f55a
Guillaume Desmarais <gui_desmarais@hotmail.com>
2019-01-11 16:15:37 -0500

    Update devoir0.js

--- devoir0.js
+++ devoir0.js
@@ -1,2 +1,10 @@
-var Guillaume = "Mauvais";
-var Nic = 'Plus Mauvais';
+//Guillaume Desmarais - p1220984
+
+var prenom = 'Guillaume';
+var nom = 'Desmarais!';
+var salutation = "Bonjour, mon nom est " + prenom + " " + nom + "!";
+var age = 22;
+var discipline = "animation3D";
+var informations = "J'ai " + age + " ans et j'étudie en " + discipline +"!";
+var introduction = salutation + "\n" + informations; 
+print(introduction);
